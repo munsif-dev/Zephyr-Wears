5d26fc13c4a0b31bc6a434b8393bc378813300eb
Munsif <munsifshaifalikhan@gmail.com>
2026-01-01 19:52:24 +0530

    upload thing updated

--- src/lib/uploadthing-components.tsx
+++ src/lib/uploadthing-components.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { generateComponents } from '@uploadthing/react';
+import { generateUploadButton, generateUploadDropzone } from '@uploadthing/react';
 import type { OurFileRouter } from './uploadthing';
 
-export const { UploadButton, UploadDropzone, Uploader } =
-  generateComponents<OurFileRouter>();
+export const UploadButton = generateUploadButton<OurFileRouter>();
+export const UploadDropzone = generateUploadDropzone<OurFileRouter>();
